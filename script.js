be06635f75e331874f442ced2a8567c55c480f32
Rohit Bhardwaj <me.rohitbhardwaj9@gmail.com>
2023-05-02 12:55:24 +0530

    commiting 2nd video content to github

---
+++ script.js
@@ -0,0 +1,13 @@
+//Typing animation
+var typed = new Typed(".typing", {
+    strings: ["Student", "Youtuber", "Blogger", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
+var typed = new Typed(".typing-2", {
+    strings: ["Student", "Youtuber", "Blogger", "Designer", "Freelancer"],
+    typeSpeed: 99,
+    backSpeed: 58,
+    loop: true
+})
